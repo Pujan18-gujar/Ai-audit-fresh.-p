496cab2d01cfebdd49402693e4b014feeadcb2c7
Pujan18-gujar <pujangujar18@gmail.com>
2026-05-26 10:29:44 +0530

    fix eslint error

--- src/Results.js
+++ src/Results.js
@@ -68,6 +68,7 @@ export default function Results({ results, onBack }) {
       }
     };
     fetchSummary();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleLeadSubmit = async () => {
